1a3d9bb0b2f73ebb8c2c478d7e987c39e2f87cea
karankhot <karan.khot@optum.com>
2022-10-06 15:11:06 -0500

    error-handling and sample test data

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const express = require('express');
 const app = express();
 const movies = require('./routes/movieRoutes');
 const connectDB = require('./db/connect');
+const notFound = require('./middleware/not-found');
+const asyncWrapper = require('./middleware/async');
+const errorHandlerMiddleware = require('./middleware/error-handler');
 
 const PORT = 3000;
 
@@ -10,6 +13,11 @@ const PORT = 3000;
 app.use(express.json());
 
 app.use('/api/v1/movies', movies);
+
+//custom-error handling middleware
+app.use(notFound);
+app.use(asyncWrapper);
+app.use(errorHandlerMiddleware);
 
 const start = async () => {
   try {
--- controllers/movies.js
+++ controllers/movies.js
@@ -1,34 +1,56 @@
 const Movie = require('../models/Movie');
+const asyncWrapper = require('../middleware/async');
+const { createCustomError } = require('../errors/custom-error');
 
-const getAllMovies = async (req, res) => {
-  try {
-    const allMovies = await Movie.find({});
-    res.status(200).json({ allMovies });
-  } catch (error) {
-    res.status(500).json({ message: error });
+const getAllMovies = asyncWrapper(async (req, res) => {
+  const allMovies = await Movie.find({});
+  res.status(200).json({ allMovies });
+});
+
+const createMovie = asyncWrapper(async (req, res) => {
+  const movie = await Movie.create(req.body);
+  res.status(201).json({ movie });
+});
+
+const getMovieWithId = asyncWrapper(async (req, res, next) => {
+  const { id: movieID } = req.params;
+  const movie = await Movie.findOne({ _id: movieID });
+  if (!movie) {
+    return next(
+      createCustomError(`Resource not found with task ID: ${movieID}`, 404)
+    );
   }
-};
+  res.status(200).json({ movie });
+});
 
-const createMovie = async (req, res) => {
-  try {
-    const movie = await Movie.create(req.body);
-    res.status(201).json({ movie });
-  } catch (error) {
-    res.status(500).json({ message: error });
+const updateMovie = asyncWrapper(async (req, res) => {
+  const { id: movieID } = req.params;
+  const movie = await Movie.findOneAndUpdate({ _id: movieID }, req.body, {
+    new: true,
+    runValidators: true,
+  });
+  if (!movie) {
+    return next(
+      createCustomError(`Update Failed!for movie ID : ${movieID} , 404`)
+    );
   }
-};
 
-const getMovieWithId = (req, res) => {
-  res.status(200).json({ id: req.params.id });
-};
+  res.status(200).json({ message: movie });
+});
 
-const updateMovie = (req, res) => {
-  res.status(200).json({ id: req.params.id });
-};
-
-const deleteMovie = (req, res) => {
-  res.status(200).json({ id: req.params.id });
-};
+const deleteMovie = asyncWrapper(async (req, res) => {
+  const { id: movieID } = req.params;
+  const movie = await Movie.findOneAndDelete({ _id: movieID });
+  if (!movie) {
+    return next(
+      createCustomError(
+        `Delete Failed: Resource not found with task ID: ${movieID}`,
+        404
+      )
+    );
+  }
+  res.status(200).json({ message: `Successfully deleted ${movieID}` });
+});
 
 module.exports = {
   getAllMovies,
